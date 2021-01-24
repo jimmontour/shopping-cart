c7cdbf3164b15c7ae43e56a3557aa2fcfe165567
Jim <jimmontour@gmail.com>
2021-01-24 17:50:29 -0500

    style: :lipstick: add content and padding to about section

--- src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function About(props) {
-   return (
-      <div className="About">
-         <h1>Footer</h1>
-      </div>
-   )
-}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import '../css/About.css'
 
 export default function About(props) {
    return (
       <div className="container">
          <h1>About Porch Goose Emporium</h1>
          <p>
-            The concrete goose, also known as a porch goose or lawn goose, is a popular lawn ornament in the United States. Concrete geese reached the peak of their popularity in the 1980s,but are still common in the Midwestern United States.
+            The concrete goose, also known as a porch goose or lawn goose, is a popular lawn ornament in the United States. Concrete geese reached the peak of their popularity in the 1980s, but are still common in the Midwestern United States.
          </p>
          <p>
             It is not uncommon for owners of concrete geese to dress them in costumes for seasonal holidays, to match the weather, or just as decoration.</p>
+         <p>
+            Here at Porch Goose Emporium , we specialize in the hottest seasonal trends for Porch Goose enthusiast.  We guarantee that your goose will be the best looking goose on the block, or your money back!
+            </p>
+
       </div>
       )
 }
