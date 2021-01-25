8a3b907b5d90cbc700f7cf872d0c1a6fd9970744
Jim <jimmontour@gmail.com>
2021-01-25 17:41:13 -0500

    style: :lipstick: add image on checkout page

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../css/checkout.css'
+import jim from '../img/jim-and-lucy.jpg'
 
 export default function CheckOut() {
    return (
       <div className="checkout-container">
-         <h2>You've Reached The End!</h2>
+         <h1>You've Reached The End!</h1>
          <p>Thank you for 'checking out' my React Shopping Cart project.  To see more of my work, visit my <a href="http://www.github.com/jimmontour">GitHub Page</a> or my <a href="http://www.jimmontour.com">website</a>.</p>
+         <img src={jim} alt="Jim and his porch goose Lucy"/>
       </div>
    )
 }
